3ad679d9e45cd63eef5755195a9c6705e923ba37
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-09-19 09:46:08 -0300

    feat: Implementing main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import styles from "./page.module.css";
+import Header from "@/components/Header";
+import TaskList from "@/components/TaskList";
+import Button from "@/components/ui/Button";
+import styles from "@/styles/page.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      
+      <Header/>
+      <main className={styles.mainContent}>
+        <TaskList />
+      </main>
+      <Button />
     </div>
   );
 }
