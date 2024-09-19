f1ca2fef1f0912550ae735a7add18c0c1c9178d6
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-09-19 16:35:01 -0300

    style: Center header styles on larger screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,16 +4,31 @@ import logo from "@/assets/logo.svg"
 import style from '@/styles/header.module.scss'
 
 function Header() {
+
+  function formatDateToLongString(date: Date) {
+      const options: Intl.DateTimeFormatOptions = { 
+          weekday: 'long', 
+          year: 'numeric', 
+          month: 'long', 
+          day: 'numeric'
+      };
+
+      return date.toLocaleDateString('pt-BR', options);
+  }
+
+  const today = new Date();
+
   return (
     <header className={style.header}>
-      <Image 
+      <Image
+        priority={true}
         src={logo}
         width={150}
         height={36} 
         alt="logo"
       />
       <h1 className={style.welcomeTitle}>Bem-vindo de volta, Marcus</h1>
-      <h2 className={style.actualDateTitle}>Segunda, 01 de dezembro de 2025</h2>
+      <h2 className={style.actualDateTitle}>{formatDateToLongString(today)}</h2>
     </header>
   )
 }
