7fdfcb01a9b805741822fc324e11c135b158bf0e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-09-19 12:05:28 -0300

    feat: Add state management for task completion

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,11 +1,28 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from '@/styles/tasks.module.scss'
 import Tasks from '@/components/ui/Taks'
-import { useTasksContext } from '@/components/ContextProvider'
+import { TaskType } from '@/types/task'
 
 function TaskList() {
-  const { tasks } = useTasksContext();
+  const [tasks, setTasks] = useState<TaskType[]>([])
+
+  const handleClickCheckTask = (taskId: string) => {
+    tasks.map((task) => {
+      if (task.id === taskId) {
+        task.completed = !task.completed
+      }
+    })
+
+    setTasks([...tasks])
+  }
+  
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem('tasks') || "[]")
+    if (tasks) {
+      setTasks(tasks)
+    }
+  }, [])
   
   const tasksCompleted = tasks.filter((task) => task.completed)
   const tasksNotCompleted = tasks.filter((task) => !task.completed)
@@ -14,11 +31,23 @@ function TaskList() {
     <>
       <h1 className={style.tasksTitle}>Suas tarefas de hoje</h1>
       {tasksNotCompleted.map((task) => {
-        return <Tasks key={task.id} checked={task.completed} description={task.description} />
+        return <Tasks 
+          key={task.id} 
+          taskId={task.id} 
+          checked={task.completed} 
+          description={task.description}
+          handleClickCheckTask={handleClickCheckTask}
+        />
       })}
       <h1 className={style.tasksTitleTasksFinished}>Tarefas finalizadas</h1>
       {tasksCompleted.map((task) => {
-        return <Tasks key={task.id} checked={task.completed} description={task.description} />
+        return <Tasks 
+          key={task.id} 
+          taskId={task.id} 
+          checked={task.completed} 
+          description={task.description}
+          handleClickCheckTask={handleClickCheckTask}
+        />
       })}
      
     </>
