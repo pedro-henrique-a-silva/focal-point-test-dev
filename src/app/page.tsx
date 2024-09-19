b11d8dc5bcba2b7b07c4d5e18c5136bae8041ec7
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-09-19 11:59:41 -0300

    feat: Add NewTaskButton component and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import Header from "@/components/Header";
 import TaskList from "@/components/TaskList";
-import Button from "@/components/ui/Button";
 import styles from "@/styles/page.module.scss";
+import NewTaskButton from "@/components/ui/NewTaskButton";
 
 export default function Home() {
+
   return (
     <div className={styles.page}>
       <Header/>
       <main className={styles.mainContent}>
         <TaskList />
       </main>
-      <Button />
+      <NewTaskButton/>
     </div>
   );
 }
