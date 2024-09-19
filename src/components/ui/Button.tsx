8664d204fa41b0f7e0e8253c15c8e8ad4f34f202
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-09-19 11:57:58 -0300

    feat: Update Button component styles to use deleteButton class

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,36 +1,27 @@
-'use client'
 import React from 'react'
 import style from "@/styles/button.module.scss"
-import { useRouter } from 'next/navigation'
 
 const buttonsStyles = {
   normal: style.normalButton,
   light: style.lightButton,
-  cancel: style.cancelButton,
+  delete: style.deleteButton,
 }
 
 type ButtonProps = {
-  buttonType?: 'normal' | 'light' | 'cancel'
+  buttonType?: 'normal' | 'light' | 'delete'
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 function Button(props: ButtonProps) {
-  const { buttonType } = props
+  const { buttonType, children, ...restOfProps } = props
 
   const buttonStyle = buttonsStyles[buttonType || 'normal']
 
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push('/new-task');
-  };
-
   return (
     <button
-      {...props}
+      {...restOfProps}
       className={`${style.button} ${buttonStyle}`}
-      onClick={handleClick}
     >
-      Adicionar nova Tarefa
+      {children}
     </button>
   )
 }
